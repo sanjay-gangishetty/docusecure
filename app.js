eaea592109edf44e407aa034a5f56b7b29fa5cb1
gangishettysanjaykumar <gangishettysanjaykumar@gmail.com>
2022-11-22 16:26:28 +0530

    added gitignore file and updated port

--- app.js
+++ app.js
@@ -189,6 +189,10 @@ app.post("/login", function(req, res) {
 });
 
 let port = process.env.PORT;
-app.listen(port||3000, function () {
-  console.log("Server started on port 3000");
+if(port == null || port == ""){
+  port = 3000;
+}
+
+app.listen(port, function () {
+  console.log("Server has started Successfully.");
 });
